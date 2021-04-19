c73c67d9bba89cf154d44f786b634253e509c5d1
pamelasamantha <p.samantha32@protonmail.com>
2021-04-19 15:40:25 -0500

    Fibonacci program

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,10 +2,42 @@ package week2;
 
 import acm.program.*;
 
+/**The program displays the Fibonacci sequence up to 
+ * where every subsequent term is the sum of the preceding 
+ * two terms up until the 
+ * MAX_TERM_VALUE = 10,000
+ * @author Pamela
+ */
+
+
 public class Fibonacci extends ConsoleProgram {
+	
+	private static final int MAX_TERM_VALUE = 10000;
+	
 
 	
 	public void run() {
-
+		int firstFib = 0;
+		int secondFib = 1;
+	
+	
+	
+	while (firstFib < MAX_TERM_VALUE) {
+		System.out.println(firstFib); 
+		int sum = firstFib + secondFib;
+		firstFib = secondFib; 
+		secondFib = sum; 
+		
 	}
+	
+		
+		
+	}
+	
+
 }
+
+
+
+
+
