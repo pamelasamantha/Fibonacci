0e64fa39137b7bb23fc31d9bb1556c97c5ccbae4
pamelasamantha <p.samantha32@protonmail.com>
2021-05-03 17:33:32 -0500

    BankApp #2

---
+++ assignment2/CDOffering.java
@@ -0,0 +1,38 @@
+package com.meritamerica.assignment2;
+
+	
+
+public class CDOffering {
+	
+		
+	int year;
+	double interestRate;
+	
+	public CDOffering(int term, double interestRate) {
+		this.year = term;
+		this.interestRate = interestRate;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+
+
+	
+	public int getTerm() {
+		return year;
+	}
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
